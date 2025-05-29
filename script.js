fc11392a128eb7b91596540db98af27d47a7fef1
narcisse-noire <emmarivalain@gmail.com>
2025-05-29 13:40:24 +0200

    script.js

---
+++ script.js
@@ -0,0 +1,11 @@
+//Sélection des éléments
+
+// Fonction pour ouvrir la modal
+
+// Fonction pour fermer la modal
+
+// Evènement de fermeture lors du clic sur le bouton fermeture
+
+//Evènement de fermeture lors du clic en dehors de la modal
+
+// Ajout des évènements de clic sur les miniatures 
